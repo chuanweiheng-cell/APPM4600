ffa6bf270192bc773a6986e4978e6a4b62c2d9d9
chuanweiheng-cell <chuanwei.heng@gmail.com>
2026-09-03 23:25:23 -0600

    homework 1

--- homework/hw1.py
+++ homework/hw1.py
@@ -29,3 +29,40 @@ y2 = f2(x)
 plt.plot(x, y2, label='$f_{2}(x) = x^9 - ... - 512$')
 plt.legend()
 plt.show()
+
+#%% q5
+
+# part (b)
+def f1(x, delta):
+    return np.cos(x + delta) - np.cos(x)
+
+def f2(x, delta):
+    return -2 * np.sin(x + delta / 2) * np.sin(delta / 2)
+
+x_1 = np.pi
+x_2 = 1e6
+
+deltas = np.logspace(-16, 0, 100)
+y_1 = f1(x_1, deltas) - f2(x_1, deltas)
+y_2 = f1(x_2, deltas) - f2(x_2, deltas)
+plt.figure(figsize=(8, 6))
+plt.semilogx(deltas, y_1, label='$x = \pi$')
+plt.semilogx(deltas, y_2, label=f'$x = {x_2:.1e}$')
+plt.xlabel('$\delta$')
+plt.ylabel('Difference between $f_1$ and $f_2$')
+plt.legend(frameon=False)
+plt.show()
+
+# part (c)
+def f3(x, delta):
+    return -delta * np.sin(x) 
+
+y_3 = f2(x_1, deltas) - f3(x_1, deltas)
+y_4 = f2(x_2, deltas) - f3(x_2, deltas)
+plt.figure(figsize=(8, 6))
+plt.semilogx(deltas, y_3, label='$x = \pi$')
+plt.semilogx(deltas, y_4, label=f'$x = {x_2:.1e}$')
+plt.xlabel('$\delta$')
+plt.ylabel('Difference between $f_2$ and $f_3$')
+plt.legend(frameon=False)
+plt.show()
